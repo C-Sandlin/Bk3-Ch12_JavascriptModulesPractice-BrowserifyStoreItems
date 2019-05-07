4ba76d35e531b49e9ae728590ec178f1fe543f7c
Colin Sandlin <csandlin010@gmail.com>
2019-05-07 15:19:12 -0500

    some minor flexbox changes

--- src/scripts/SingleProduct.js
+++ src/scripts/SingleProduct.js
@@ -5,7 +5,7 @@ const displaySingleProduct = productId => {
     const renderHere = document.querySelector(".product-location");
     renderHere.innerHTML += `
     <div id="product-container">
-      <div id="product-${productId}">
+      <div id="product-${productId}" class="single-item-focus">
         <img src="${product.image}"></img>
         <h1>${product.product_name}</h1>
         <h4>Price: $${product.price}</h4>
